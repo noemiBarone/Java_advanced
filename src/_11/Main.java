5473fe394be326e92cf3861d8fd155db4ddd87e0
Noemi <noemibarone00@gmail.com>
2023-05-15 10:05:48 +0200

    _11

---
+++ src/_11/Main.java
@@ -0,0 +1,52 @@
+package _11;
+
+public class Main {
+    /**
+     * 1:
+     *
+     *
+     * In the main method, create a variable x as a random number between 0-100 using Math.random() * 100;
+     *
+     * Use a ternary operator to check if x is greater than or equal to 50. If it is, print "x is greater than or equal to 50". If it's not, print "x is less than 50".
+     *
+     * Experiment with different values of x and observe the output
+     */
+    private static void exercise1() {
+        System.out.println("\nExercise 1: ");
+        // Your code here
+    }
+
+    /**
+     * 2:
+     *
+     *
+     * Create a record called "Person" with fields for name, age and address.
+     *
+     * Add a toString method to Person to print out the data in a different format
+     *
+     * Print the record to the console
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2: ");
+        // Your code here
+    }
+
+    /**
+     * 3:
+     *
+     *
+     * In the main method, create a BigInteger variable with a large value.
+     *
+     * Create a BigDecimal variable with a large value
+     *
+     * Divide the BigInteger by 3 using the divide method and assign the result to a new BigInteger variable
+     *
+     * Divide the BigDecimal by Math.PI using the divide method and assign the result to a new BigDecimal variable
+     *
+     * Print the results
+     */
+    private static void exercise3() {
+        System.out.println("\nExercise 3: ");
+        // Your code here
+    }
+}
